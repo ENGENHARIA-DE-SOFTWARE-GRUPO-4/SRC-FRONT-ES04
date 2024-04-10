6c846342eb5c9f38736feb6731428a45c9f2c23c
leonardoCavalcantii <leofilho206@gmail.com>
2024-04-10 20:45:30 -0300

    primeiras implementacoes pagina login

--- front-end/src/pages/LoginPage.js
+++ front-end/src/pages/LoginPage.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/Login.css'
-//import{Formik} from "formik";
 
-const LoginPage = ()=>{
-    return(
-        <div className = "teste">Testando</div>        
-    );
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    console.log("Email:", email);
+    console.log("Password:", password);
+    
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <h1>Login</h1>
+        <h2>Faça login pra continuar</h2>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Senha:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+      </div>
+      <button type="submit">entrar</button>
+    </form>
+  );
 };
 
-export default LoginPage;
+export default LoginForm;
