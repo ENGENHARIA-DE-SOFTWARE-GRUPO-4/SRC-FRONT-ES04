259fd70c11e9a7989e4ca0ae2d91e586f985498c
leonardoCavalcantii <leofilho206@gmail.com>
2024-04-09 23:08:51 -0300

    configuracoes de roteamento prontas e aplicacao funcionando, pastas de pages e styles separadas e funcionando roteanento entre as mesmas

---
+++ front-end/src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../styles/Login.css'
+//import{Formik} from "formik";
+
+const LoginPage = ()=>{
+    return(
+        <div className = "teste">Testando</div>        
+    );
+};
+
+export default LoginPage;
