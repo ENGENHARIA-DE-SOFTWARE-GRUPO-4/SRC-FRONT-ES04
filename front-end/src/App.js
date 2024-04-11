04e99f0c3308677d6ab7a17b5c134b88e27403cb
GustavoAlvrega <gustavo.costa@ccc.ufcg.edu.br>
2024-04-10 21:23:35 -0300

    tela de menu

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/LoginPage'
+import Menu from './pages/Menu';
 //adicionar as rotas aqui!
 function App() {
   return(
     <>
     <Routes>
       <Route path = "login" element={<LoginPage />}/>
+      <Route path = "menu" element={<Menu />}/>
     </Routes> 
     </> 
   );
---
+++ front-end/src/pages/Menu.js
@@ -0,0 +1,40 @@
+import React from "react";
+import '../styles/Menu.css'
+
+const Menu = () =>{
+    return(
+        <div>  
+            <div class="totalContainer">
+                <div class="fotoPerfil_e_Dados">
+                <div class="fotoPerfil">
+                    <img src="https://marketplace.canva.com/EAFegYHtpE4/1/0/1600w/canva-foto-de-perfil-para-o-instagram-luxuosa-e-elegante-preto-e-bege-neon-rFVNtE4jncE.jpg"/>
+                </div>
+
+                <div class="containerDados">
+                    <div class="dados">
+                        <p>Nome:</p>
+                        <p>Período:</p>
+                    </div>
+                </div>
+
+                </div>
+
+                <div class="links_Greed">
+                    <div class="links_greed_container_a">
+                        <a href="!">Meu Dashboard</a>
+                        <a href="@">Minha Agenda</a>
+                        <a href="#">Meu Calendário</a>
+                    </div>
+
+                    <div  class="links_greed_container_b">
+                        <a href="$">Fórum Online</a>
+                        <a href="%">Salas Virtuais</a>
+                        <a href="&">Sair</a>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Menu;
